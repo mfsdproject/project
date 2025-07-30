c81b1fa028b99e886fc4b93402d0bb397ede0d2d
htoomyattun <40516588@live.napier.ac.uk>
2025-07-30 04:19:27 +0100

    signin

--- middleware.ts
+++ middleware.ts
@@ -1,26 +1,3 @@
-/*
---- 2. File: middleware.ts ---
-Place this at the root of your project. This protects all routes
-except for the home page, which is our sign-in page.
-*/
-
-import { clerkMiddleware } from "@clerk/nextjs/server";
-
-export default clerkMiddleware({
-  publicRoutes: ["/"],
-});
-
-export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
-    "/(api|trpc)(.*)",
-  ],
-};
-
-
-
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
 export default clerkMiddleware();
